2e580216c9a45d5157126aa96ae9ca969c4047d2
Azhar730 <azharmahmud730@gmail.com>
2024-06-21 22:14:40 +0600

    add publisher section done

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -3,11 +3,11 @@ import useAuth from "../Hooks/useAuth";
 import toast from "react-hot-toast";
 
 const Profile = () => {
-    const { user, updateUserProfile, update, setUpdate } = useAuth()
-    const navigate = useNavigate()
-    const from = '/profile'
-    const handleUpdate = async e => {
-        e.preventDefault()
+    // const { user, updateUserProfile, update, setUpdate } = useAuth()
+    // const navigate = useNavigate()
+    // const from = '/profile'
+    // const handleUpdate = async e => {
+    //     e.preventDefault()
         const form = e.target;
         const fullName = form.fullName.value;
         const photo = form.photo.value
