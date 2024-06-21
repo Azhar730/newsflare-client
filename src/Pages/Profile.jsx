6fefdb14463531f00dd88c2c24f7611a52f908d8
Azhar730 <azharmahmud730@gmail.com>
2024-06-21 22:13:42 +0600

    navbar section done

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -3,9 +3,9 @@ import useAuth from "../Hooks/useAuth";
 import toast from "react-hot-toast";
 
 const Profile = () => {
-    // const { user, updateUserProfile, update, setUpdate } = useAuth()
-    // const navigate = useNavigate()
-    // const from = '/profile'
+    const { user, updateUserProfile, update, setUpdate } = useAuth()
+    const navigate = useNavigate()
+    const from = '/profile'
     const handleUpdate = async e => {
         e.preventDefault()
         const form = e.target;
