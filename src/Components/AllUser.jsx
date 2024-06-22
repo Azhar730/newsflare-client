c3e4ef2e039d5f913a8419bdfc6e82d23051d644
Azhar730 <azharmahmud730@gmail.com>
2024-06-22 20:14:22 +0600

    user with countup

--- src/Components/AllUser.jsx
+++ src/Components/AllUser.jsx
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { axiosCommon } from "../Hooks/useAxiosCommon";
+import CountUp from 'react-countup';
 
 const AllUser = () => {
     const { data: users = [] } = useQuery({
@@ -10,10 +11,43 @@ const AllUser = () => {
         }
     })
     return (
-        <div>
-            <p>All User{users.users}</p>
-            <p>Normal User{users.normalUsers}</p>
-            <p>Premium User{users.premiumUsers}</p>
+        <div className="text-center gap-x-20 text-3xl font-bold flex items-center justify-center">
+            <p className="bg-gray-200 px-6 py-2 shadow-xl rounded-xl">All User <br /> <CountUp
+                start={0}
+                end={users.users}
+                duration={.75}
+            >
+                {({ countUpRef, start }) => (
+                    <div>
+                        <span ref={countUpRef} />
+                        <button className="btn btn-sm bg-blue-400 hover:bg-blue-500" onClick={start}>Start</button>
+                    </div>
+                )}
+            </CountUp></p>
+            <p className="bg-gray-200 px-6 py-2 shadow-xl rounded-xl">Normal User <br /> <CountUp
+                start={0}
+                end={users.normalUsers}
+                duration={.75}
+            >
+                {({ countUpRef, start }) => (
+                    <div>
+                        <span ref={countUpRef} />
+                        <button className="btn btn-sm bg-blue-400 hover:bg-blue-500" onClick={start}>Start</button>
+                    </div>
+                )}
+            </CountUp></p>
+            <p className="bg-gray-200 px-6 py-2 shadow-xl rounded-xl">Premium User <br /> <CountUp
+                start={0}
+                end={users.premiumUsers}
+                duration={.75}
+            >
+                {({ countUpRef, start }) => (
+                    <div>
+                        <span ref={countUpRef} />
+                        <button className="btn btn-sm bg-blue-400 hover:bg-blue-500" onClick={start}>Start</button>
+                    </div>
+                )}
+            </CountUp></p>
         </div>
     );
 };
