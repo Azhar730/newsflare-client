9d4b88da15b47fd70a6e9bc04b5c8560099f9a05
Azhar730 <azharmahmud730@gmail.com>
2024-06-22 20:23:06 +0600

    footer done

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../Components/NavBar";
+import Footer from "../Components/Footer";
 
 const MainLayout = () => {
     return (
         <div className="max-w-6xl mx-auto">
             <NavBar/>
             <Outlet/>
+            <Footer/>
         </div>
     );
 };
